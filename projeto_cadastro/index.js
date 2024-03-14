6466e7d1d00efd73539e4996e2a918f9c024083e
Lucas Domingues <125484200+lucasdominguess@users.noreply.github.com>
2024-03-14 17:09:21 -0300

    mudando pasta

--- projeto_cadastro/index.js
+++ projeto_cadastro/index.js
@@ -59,9 +59,9 @@ async function listar(){
 
 
 
-  // document.body.addEventListener("pointermove", (e)=>{
-  //   const { currentTarget: el, clientX: x, clientY: y } = e;
-  //   const { top: t, left: l, width: w, height: h } = el.getBoundingClientRect();
-  //   el.style.setProperty('--posX',  x-l-w/2);
-  //   el.style.setProperty('--posY',  y-t-h/2);
-  // })
+  document.body.addEventListener("pointermove", (e)=>{
+    const { currentTarget: el, clientX: x, clientY: y } = e;
+    const { top: t, left: l, width: w, height: h } = el.getBoundingClientRect();
+    el.style.setProperty('--posX',  x-l-w/2);
+    el.style.setProperty('--posY',  y-t-h/2);
+  })
